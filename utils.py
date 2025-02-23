112979e0ca3ab1ac66305ee0850abbf64bb1a078
Romain Hu <romainhu@laposte.net>
2025-02-23 21:02:46 +0100

    Add models, utils and diff operators

---
+++ utils.py
@@ -0,0 +1,96 @@
+import torch
+import scipy.ndimage
+
+
+#####################################################################################################
+#####################################################################################################
+def get_mgrid(sidelen, dim = 2):
+    '''
+    Returns a flattened grid of (x,y,...) coordinates in a range of -1 to 1.
+    sidelen: int
+    dim: int
+    '''
+    tensors = tuple(dim * [torch.linspace(-1, 1, steps=sidelen)])
+    mgrid = torch.stack(torch.meshgrid(*tensors), dim=-1)
+    mgrid = mgrid.reshape(-1, dim)
+    return mgrid
+
+
+#####################################################################################################
+#####################################################################################################
+def get_cameraman_tensor(sidelength):
+    '''
+    '''
+    img = Image.fromarray(skimage.data.camera())        
+    transform = Compose([
+        Resize(sidelength),
+        ToTensor(),
+        Normalize(torch.Tensor([0.5]), torch.Tensor([0.5]))
+    ])
+    img = transform(img)
+    return img
+
+
+#####################################################################################################
+#####################################################################################################
+class ImageFitting(Dataset):
+    '''
+    '''
+    def __init__(self, sidelength):
+        super().__init__()
+        img = get_cameraman_tensor(sidelength)
+        self.pixels = img.permute(1, 2, 0).view(-1, 1)
+        self.coords = get_mgrid(sidelength, 2)
+
+    def __len__(self):
+        return 1
+
+    def __getitem__(self, idx):    
+        if idx > 0: raise IndexError
+        return self.coords, self.pixels
+
+
+#####################################################################################################
+#####################################################################################################
+class PoissonEqn(Dataset):
+    '''
+    '''
+    def __init__(self, sidelength):
+        super().__init__()
+        img = get_cameraman_tensor(sidelength)
+        
+        # Compute gradient and laplacian       
+        grads_x = scipy.ndimage.sobel(img.numpy(), axis=1).squeeze(0)[..., None]
+        grads_y = scipy.ndimage.sobel(img.numpy(), axis=2).squeeze(0)[..., None]
+        grads_x, grads_y = torch.from_numpy(grads_x), torch.from_numpy(grads_y)
+                
+        self.grads = torch.stack((grads_x, grads_y), dim=-1).view(-1, 2)
+        self.laplace = scipy.ndimage.laplace(img.numpy()).squeeze(0)[..., None]
+        self.laplace = torch.from_numpy(self.laplace)
+        
+        self.pixels = img.permute(1, 2, 0).view(-1, 1)
+        self.coords = get_mgrid(sidelength, 2)
+
+    def __len__(self):
+        return 1
+
+    def __getitem__(self, idx):
+        return self.coords, {'pixels':self.pixels, 'grads':self.grads, 'laplace':self.laplace}
+
+
+#####################################################################################################
+#####################################################################################################
+def gradients_mse(model_output, coords, gt_gradients):
+    '''
+    Returns the mean squared error between the gradients of the model and
+    the ground-truth gradients.
+
+    - model_output: torch.Tensor
+    - coords: torch.Tensor
+    - gt_gradients: torch.Tensor
+    '''
+    # compute gradients on the model
+    gradients = gradient(model_output, coords)
+    # compare them with the ground-truth
+    gradients_loss = torch.mean((gradients - gt_gradients).pow(2).sum(-1))
+    return gradients_loss
