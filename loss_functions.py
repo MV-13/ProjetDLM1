6c85895dd7bb6739f30324d40ae7376c1a68457f
Romain Hu <romainhu@laposte.net>
2025-02-24 15:37:36 +0100

    add wave equation loss function and dataset

--- loss_functions.py
+++ loss_functions.py
@@ -25,12 +25,36 @@ def gradients_mse(model_output, coords, gt_gradients):
     - coords: torch.Tensor ;
     - gt_gradients: torch.Tensor, ground-truth gradients.
     '''
-    # compute gradients on the model
+    # Compute gradients on the model
     gradients = diff.gradient(model_output, coords)
-    # compare them with the ground-truth
+
+    # Compare them with the ground-truth
     gradients_loss = torch.mean((gradients - gt_gradients).pow(2).sum(-1))
+
     return gradients_loss
 
 
 #####################################################################################################
 #####################################################################################################
+def wave_loss(x, y, t, lambda1, lambda2):
+    '''
+    Loss function for the wave equation with initial conditions.
+
+    - x : torch.Tensor, spatial coordinates ;
+    - y : torch.Tensor, output of the model ;
+    - t : float, if t = 0.0, add initial condition constraints to the loss ;
+    - lambda1 : float, weight for the dirichlet constraints, recommended value : batch size/100 ;
+    - lambda2 : float, weight for the neuman constraints, recommended value : batch size/10.
+    '''
+    grad = diff.jacobian(y, x) # First derivative.
+    hess = diff.jacobian(grad[..., 0, :], x) # Shape (..., y components, x components)
+    laplacian = hess[..., 1, 1, None] + hess[..., 2, 2, None]
+    grad_t2 = hess[..., 0, 0, None] # Second derivates w.r.t. time.
+    loss = grad_t2 - laplacian # Wave equation constraint.
+
+    if torch.all(t == 0.):
+        dirichlet_loss = y # Needs modification.
+        neumann_loss = grad[..., 0]
+        loss += dirichlet_loss + neumann_loss
+
+    return loss
--- utils.py
+++ utils.py
@@ -120,3 +120,63 @@ def display_img(epoch, step_til_summary, output, coords, loss, size, device):
         axes[1].imshow(grad.to(device).norm(dim = -1).to(device).view(size, size).detach().numpy())
         axes[2].imshow(laplacian.view(size, size).detach().numpy())
         plt.show()
+
+
+#####################################################################################################
+#####################################################################################################
+class WaveSource(Dataset):
+    '''
+    Returns the coordinates and source boundary values for the wave equation.
+    '''
+    def __init__(self, sidelength, source_coords = [0., 0.]):
+        super().__init__()
+        torch.manual_seed(0)
+        self.sidelength = sidelength
+        self.mgrid = get_mgrid(self.sidelength).detach()
+        self.N_src_samples = 1000
+        self.sigma = 5e-4
+        self.source_coords = torch.tensor(source_coords).view(-1, 3)
+        self.counter = 0
+        self.full_count = 100e3
+
+    def __len__(self):
+        return 1
+
+    def __getitem__(self, idx):
+        start_time = self.source_coords[0, 0]  # Time to apply  initial conditions.
+
+        r = 5e2 * self.sigma * torch.rand(self.N_src_samples, 1).sqrt()
+        phi = 2 * np.pi * torch.rand(self.N_src_samples, 1)
+
+        # Circular sampling.
+        source_coords_x = r * torch.cos(phi) + self.source_coords[0, 1]
+        source_coords_y = r * torch.sin(phi) + self.source_coords[0, 2]
+        source_coords = torch.cat((source_coords_x, source_coords_y), dim=1)
+
+        # uniformly sample domain and include coordinates where source is non-zero 
+        coords = torch.zeros(self.sidelength ** 2, 2).uniform_(-1, 1)
+
+        # slowly grow time values from start time
+        # this currently assumes start_time = 0 and max time value is 0.75. 
+        time = torch.zeros(self.sidelength ** 2, 1).uniform_(0, 0.4 * (self.counter / self.full_count))
+        coords = torch.cat((time, coords), dim=1)
+
+        # make sure we always have training samples at the initial condition
+        coords[-self.N_src_samples:, 1:] = source_coords
+        coords[-2 * self.N_src_samples:, 0] = start_time
+
+        # set up source
+        normalize = 50 * gaussian(torch.zeros(1, 2), mu=torch.zeros(1, 2), sigma=self.sigma, d=2)
+        boundary_values = gaussian(coords[:, 1:], mu=self.source_coords[:, 1:], sigma=self.sigma, d=2)[:, None]
+        boundary_values /= normalize
+
+        # only enforce initial conditions around start_time
+        boundary_values = torch.where((coords[:, 0, None] == start_time), boundary_values, torch.Tensor([0]))
+        dirichlet_mask = (coords[:, 0, None] == start_time)
+
+        boundary_values[boundary_values < 1e-5] = 0.
+
+        self.counter += 1
+
+        return {'coords': coords}, {'source_boundary_values': boundary_values,
+                                    'dirichlet_mask': dirichlet_mask}
